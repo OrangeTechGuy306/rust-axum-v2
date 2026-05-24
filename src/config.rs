4189625a0b06bf272f0c7d4b28ddd70eaecdf212
orangetechguy306 <orangetechguy@gmail.com>
2026-05-24 13:17:43 +0100

    create dto and set project config

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+ use std::env;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub database_url: String,
+    pub jwt_secret: String,
+    pub jwt_maxage: i64,
+    pub port: u16,
+}
+
+impl Config {
+    pub fn init() -> Config {
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is required");
+        let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET is required");
+        let jwt_maxage = env::var("JWT_MAXAGE")
+            .expect("JWT_MAXAGE is required")
+            .parse::<i64>()
+            .expect("JWT_MAXAGE must be an integer");
+
+        Config{
+            database_url,
+            jwt_secret,
+            jwt_maxage: jwt_maxage,
+            port: 8000
+        }
+    }
+ }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod models;
-
+mod config;
+mod dto;
 
 fn main() {
     println!("Hello, world!");
